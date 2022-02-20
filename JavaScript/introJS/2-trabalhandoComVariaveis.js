aae88981c806dd6deaa70bda83accd298590b306
Thiago Brito <thiago7brito@gmail.com>
2022-02-20 17:36:48 -0300

    Novos Exercícios
    2, 3 e 4

---
+++ JavaScript/introJS/2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+console.log("Trabalhando com variáveis.");
+// JS é case sensitive
+
+// Sem const muda escopo (má prática)
+const idade = 29;
+const nome = "Ricardo";
+
+// Não fazer:
+// mes = "Janeiro";
