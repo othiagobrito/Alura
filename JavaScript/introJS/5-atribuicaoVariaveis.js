3939ede8a710bce8533f12394498bbb27ae2697d
Thiago Brito <thiago7brito@gmail.com>
2022-02-20 20:00:05 -0300

    Update 5-atribuicaoVariaveis.js

--- JavaScript/introJS/5-atribuicaoVariaveis.js
+++ JavaScript/introJS/5-atribuicaoVariaveis.js
@@ -6,7 +6,7 @@ const sobrenome = "Bugan";
 
 // console.log(nome + " " + sobrenome);
 console.log(nome, sobrenome);
-console.log('Meu nome é ${nome} ${sobrenome}');
+console.log(`Meu nome é ${nome} ${sobrenome}`);
 
 const nomeCompleto = nome + sobrenome;
 console.log(nomeCompleto);
