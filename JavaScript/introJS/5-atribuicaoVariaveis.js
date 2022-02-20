49700fa68935c609cc41637a6a42be3e4f7d6d33
Thiago Brito <thiago7brito@gmail.com>
2022-02-20 18:03:38 -0300

    Create 5-atribuicaoVariaveis.js

---
+++ JavaScript/introJS/5-atribuicaoVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com atribuição de variáveis.");
+
+const idade =  29;
+const nome = "Ricardo";
+const sobrenome = "Bugan";
+
+// console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log('Meu nome é ${nome} ${sobrenome}');
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
