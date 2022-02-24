69acb2d7fd080726d155f114d4eaaaba945de0bd
Thiago Brito <thiago7brito@gmail.com>
2022-02-24 17:38:57 -0300

    Files update

--- JavaScript/introJS-Web/js/principal.js
+++ JavaScript/introJS-Web/js/principal.js
@@ -1,5 +1,35 @@
-console.log("Carregado de um arquivo externo.");
-
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var imc = paciente.querySelector(".info-imc");
+
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido.")
+        pesoValido = false;
+        imc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.0) {
+        console.log("Altura inválida.")
+        alturaValida = false;
+        imc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaValida && pesoValido) {
+        imc.textContent = (peso / (altura * altura)).toFixed(2);
+    }
+}
