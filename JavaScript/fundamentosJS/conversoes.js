169e77ce41ec2cc59a7cac8b032c8b0aef523489
Thiago Brito <thiago7brito@gmail.com>
2022-03-03 13:12:59 -0300

    New files

---
+++ JavaScript/fundamentosJS/conversoes.js
@@ -0,0 +1,14 @@
+// tipode de dado
+// booleanos
+
+// conversão implícita
+const n1 = 456;
+const n2 = "456";
+
+console.log(n1 == n2);
+
+// Number()
+// String()
+
+// conversão explícita
+console.log(n1 + Number(n2));
---
+++ JavaScript/fundamentosJS/truthy-falsy.js
@@ -0,0 +1,26 @@
+// Boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+// Truthy or falsy
+
+// 0 -< false
+// 1 -> true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+// undefined
+// null -> vazio ou nada
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let num = 3;
+let txt = "Alura";
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
