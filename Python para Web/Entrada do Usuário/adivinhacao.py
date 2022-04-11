5f9518d29f90d46a03102752a5130d59daebdcbd
Thiago Brito <thiago7brito@gmail.com>
2022-04-11 11:22:11 -0300

    5.1
    O laço com for

--- Python para Web/Entrada do Usuário/adivinhacao.py
+++ Python para Web/Entrada do Usuário/adivinhacao.py
@@ -1,3 +1,6 @@
+import atexit
+
+
 print("*********************************")
 print("Bem Vindo ao jogo de Adivinhação!")
 print("*********************************")
@@ -6,8 +9,8 @@ secret_number = 43
 
 attempts = 3
 
-while attempts > 0:
-    print(f"\nVocê tem {attempts} tentativas\n")
+for round in range(1, attempts + 1):
+    print(f"\nTentativa {round} de {attempts}\n")
 
     guess = int(input("Digite o seu número: "))
 
@@ -25,7 +28,5 @@ while attempts > 0:
             print("Você errou! Seu chute foi maior do que o número secreto.\n")
         else:
             print("Você errou! Seu chute foi menor do que o número secreto.\n")
-    
-    attempts -= 1
 
 print("\nFim do jogo!")
