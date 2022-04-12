8060ef8d4e21b845cd9fbfc8547b8b9eb1fdea01
Thiago Brito <thiago7brito@gmail.com>
2022-04-12 12:41:43 -0300

    2.3
    Construtor

--- Python para Web/Orientação a Objetos/conta.py
+++ Python para Web/Orientação a Objetos/conta.py
@@ -1,2 +1,8 @@
 class Account:
-    pass
+    def __init__(self, number, holder, balance, limit):
+        print(f"Construindo objeto ... {self}")
+
+        self.number = number
+        self.holder = holder
+        self.balance = balance
+        self.limit = limit
