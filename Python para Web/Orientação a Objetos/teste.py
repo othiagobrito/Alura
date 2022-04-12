ad32b8242fd360947f5c94c7f0b5dbb78c243cdd
Thiago Brito <thiago7brito@gmail.com>
2022-04-12 10:36:34 -0300

    2.1
    Dados da conta

---
+++ Python para Web/Orientação a Objetos/teste.py
@@ -0,0 +1,9 @@
+def create_account(numero, titular, saldo, limite):
+    conta = {
+        "numero": numero,
+        "titular": titular,
+        "saldo": saldo,
+        "limite": limite
+    }
+
+    return conta
