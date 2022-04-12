47b244d77d4ef231f96c6aa3ee04a9864461ce07
Thiago Brito <thiago7brito@gmail.com>
2022-04-12 08:52:17 -0300

    5.1
    Estipulando tentativas de erros

--- Python para Web/Conhecer a linguagem/forca.py
+++ Python para Web/Conhecer a linguagem/forca.py
@@ -12,6 +12,9 @@ def jogar():
     game_over = False
     right_word = False
 
+    wrong_attempts = 0
+    attempts_limit = 6
+
     print(f"\nA Palavra, a ser adivinhada, possui {len(secret_word)} letras:")
     while not game_over and not right_word:
         word = "".join(right_letters)
@@ -20,13 +23,27 @@ def jogar():
         guess = input("Escolha uma letra: ").lower().strip()
         print()
 
-        index = 0
-        for letter in secret_word:
-            if guess == letter:
-                right_letters[index] = guess
-            index += 1
+        if guess in secret_word:
+            index = 0
+            for letter in secret_word:
+                if guess == letter:
+                    right_letters[index] = guess
+                index += 1
+        else:
+            wrong_attempts += 1
+            print(f"A letra '{guess.upper()}' não consta na palavra.\nVocê tem mais {attempts_limit - wrong_attempts} tentativas")
+        
+        if wrong_attempts == attempts_limit:
+            print("\nGAME OVER!")
+            game_over = True
+        
+        if "_" not in right_letters:
+            word = "".join(right_letters)
+            print(f"\n{word}\n")
+            print("Você acertou a palavra secreta!!")
+            right_word = True
 
-    print("\nFim do jogo!")
+    print("Fim do jogo!")
 
 if __name__ == "__main__":
     jogar()
