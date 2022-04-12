aeb3634e7f4e2a2f3648890f7412c8143077cf54
Thiago Brito <thiago7brito@gmail.com>
2022-04-12 13:20:33 -0300

    4.1
    Atributos privados

--- Python para Web/Orientação a Objetos/conta.py
+++ Python para Web/Orientação a Objetos/conta.py
@@ -5,19 +5,19 @@ class Account:
     def __init__(self, number, holder, balance, limit):
         print(f"Construindo objeto ... {self}")
 
-        self.number = number
-        self.holder = holder
-        self.balance = balance
-        self.limit = limit
+        self.__number = number
+        self.__holder = holder
+        self.__balance = balance
+        self.__limit = limit
 
     def statement(self):
-        print(f"Saldo de {self.holder}: R$ {self.balance:.2f}")
+        print(f"Saldo de {self.__holder}: R$ {self.__balance:.2f}")
     
     def deposit(self, value):
-        self.balance += value
+        self.__balance += value
 
     def withdraw(self, value):
-        self.balance -= value
+        self.__balance -= value
 
 acc = Account(123, "Thiago", 1500.0, 1000.0)
 acc.statement()
