b159a965988e0caa4b1ae0537c9319784e3b2be7
Thiago Brito <thiago7brito@gmail.com>
2022-04-11 21:43:49 -0300

    2.1
    Encontrando letras

--- Python para Web/Conhecer a linguagem/forca.py
+++ Python para Web/Conhecer a linguagem/forca.py
@@ -9,7 +9,14 @@ def jogar():
     right_word = False
 
     while not game_over and not right_word:
-        pass
+        guess = input("Escolha uma letra: ").lower()
+        print()
+
+        index = 0
+        for letter in secret_word:
+            if guess == letter:
+                print(f"Letra '{guess}' encontrada na posição {index}")
+            index += 1
 
     print("\nFim do jogo!")
 
