93073f3a96d56eb109226912a0b93a28ba4dd62a
Thiago Brito <thiago7brito@gmail.com>
2022-04-11 22:26:51 -0300

    3.6
    Guardando as letras acertadas

--- Python para Web/Conhecer a linguagem/forca.py
+++ Python para Web/Conhecer a linguagem/forca.py
@@ -4,18 +4,26 @@ def jogar():
     print("*********************************")
 
     secret_word = "banana"
+    right_letters = []
+    
+    for letter in secret_word:
+        right_letters.append(letter.replace(letter, "_"))
 
     game_over = False
     right_word = False
 
+    print(f"\nA Palavra, a ser adivinhada, possui {len(secret_word)} letras:")
     while not game_over and not right_word:
+        word = "".join(right_letters)
+        print(f"\n{word}\n")
+
         guess = input("Escolha uma letra: ").lower().strip()
         print()
 
         index = 0
         for letter in secret_word:
             if guess == letter:
-                print(f"Letra '{guess}' encontrada na posição {index}")
+                right_letters[index] = guess
             index += 1
 
     print("\nFim do jogo!")
