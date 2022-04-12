cbb57d087e5c82a31fc09c40475c4aa19b921f00
Thiago Brito <thiago7brito@gmail.com>
2022-04-12 13:01:01 -0300

    3.1
    Usando métodos

--- Python para Web/Orientação a Objetos/conta.py
+++ Python para Web/Orientação a Objetos/conta.py
@@ -1,3 +1,6 @@
+from teste import statement
+
+
 class Account:
     def __init__(self, number, holder, balance, limit):
         print(f"Construindo objeto ... {self}")
@@ -6,3 +9,21 @@ class Account:
         self.holder = holder
         self.balance = balance
         self.limit = limit
+
+    def statement(self):
+        print(f"Saldo de {self.holder}: R$ {self.balance:.2f}")
+    
+    def deposit(self, value):
+        self.balance += value
+
+    def withdraw(self, value):
+        self.balance -= value
+
+acc = Account(123, "Thiago", 1500.0, 1000.0)
+acc.statement()
+
+acc.deposit(1000)
+acc.statement()
+
+acc.withdraw(30)
+acc.statement()
