01505005ed334e9d1abe04be914795775129d98f
Thiago Brito <thiago7brito@gmail.com>
2022-04-13 10:13:43 -0300

    1.6
    Encapsulando comportamento

--- Python para Web/Avançando na Orientação a Objetos/model.py
+++ Python para Web/Avançando na Orientação a Objetos/model.py
@@ -1,22 +1,46 @@
 class Movie:
     def __init__(self, name, year, lenght):
-        self.name = name.title()
+        self.__name = name.title()
         self.year = year
         self.lenght = lenght
-        self.likes = 0
+        self.__likes = 0
     
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, name):
+        self.__name = name.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
     def like(self):
-        self.likes += 1
+        self.__likes += 1
 
 class Series:
     def __init__(self, name, year, seasons):
-        self.name = name.title()
+        self.__name = name.title()
         self.year = year
         self.seasons = seasons
-        self.likes = 0
+        self.__likes = 0
     
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, name):
+        self.__name = name.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
     def like(self):
-        self.likes += 1
+        self.__likes += 1
 
 avengers = Movie("Avengers - Infinite War", 2018, 160)
 avengers.like()
