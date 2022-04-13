93a5ec8fa101324374489dd3803f235de347c9a6
Thiago Brito <thiago7brito@gmail.com>
2022-04-13 10:07:15 -0300

    1.4
    Adicionando atributos e métodos

--- Python para Web/Avançando na Orientação a Objetos/model.py
+++ Python para Web/Avançando na Orientação a Objetos/model.py
@@ -1,17 +1,28 @@
 class Movie:
     def __init__(self, name, year, lenght):
-        self.name = name
+        self.name = name.title()
         self.year = year
         self.lenght = lenght
+        self.likes = 0
+    
+    def like(self):
+        self.likes += 1
 
 class Series:
     def __init__(self, name, year, seasons):
-        self.name = name
+        self.name = name.title()
         self.year = year
         self.seasons = seasons
+        self.likes = 0
+    
+    def like(self):
+        self.likes += 1
 
-avangers = Movie("Avengers - Infinite War", 2018, 160)
-print(f"Nome: {avangers.name} - Year: {avangers.year} - Lenght: {avangers.lenght}m")
+avengers = Movie("Avengers - Infinite War", 2018, 160)
+avengers.like()
+print(f"Nome: {avengers.name} - Year: {avengers.year} - Lenght: {avengers.lenght}m - Likes: {avengers.likes}")
 
 atlanta = Series("Atlanta", 2018, 2)
-print(f"Nome: {atlanta.name} - Year: {atlanta.year} - Seasons: {atlanta.seasons}")
+atlanta.like()
+atlanta.like()
+print(f"Nome: {atlanta.name} - Year: {atlanta.year} - Seasons: {atlanta.seasons} - Likes: {atlanta.likes}")
