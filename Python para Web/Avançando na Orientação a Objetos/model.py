a13e37ce7b531e3008c803c56eea8e5107543f4c
Thiago Brito <thiago7brito@gmail.com>
2022-04-13 19:03:11 -0300

    4.6
    Fugindo da complexidade

--- Python para Web/Avançando na Orientação a Objetos/model.py
+++ Python para Web/Avançando na Orientação a Objetos/model.py
@@ -38,10 +38,18 @@ class Series(Program):
     def __str__(self):
         return f"{self._name} - {self.year} - {self.seasons} seasons - {self._likes} likes"
 
-class Playlist(list):
+class Playlist:
     def __init__(self, name, programs):
         self.name = name
-        super().__init__(programs)
+        self._programs = programs
+
+    @property
+    def listing(self):
+        return self._programs
+    
+    @property
+    def size(self):
+        return len(self._programs)
 
 avengers = Movie("Avengers - Infinite War", 2018, 160)
 batman = Movie("The Batman", 2022, 176)
@@ -63,9 +71,9 @@ young_sheldon.like()
 movies_and_series = [avengers, atlanta, batman, young_sheldon]
 weekend_playlist = Playlist("weekend", movies_and_series)
 
-print(f"Playlist size: {len(weekend_playlist)}")
+print(f"Playlist size: {weekend_playlist.size}")
 
-for program in weekend_playlist:
+for program in weekend_playlist.listing:
     print(program)
 
-print(f"In playlist: {batman in weekend_playlist}")
+print(f"In playlist: {batman in weekend_playlist.listing}")
