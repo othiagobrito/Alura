07e20ca9513a93798001462f939e969bbe333520
Thiago Brito <thiago7brito@gmail.com>
2022-04-17 11:20:27 -0300

    4.3
    Quantificadores e intervalos

--- Python para Web/String em Python Com OO/url-extractor/cep-extractor.py
+++ Python para Web/String em Python Com OO/url-extractor/cep-extractor.py
@@ -1,8 +1,8 @@
 import re
 
-address = "Rua das Flores 72, Apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440120"
+address = "Rua das Flores 72, Apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120"
 
-pattern = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+pattern = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 search = pattern.search(address)
 
 if search:
