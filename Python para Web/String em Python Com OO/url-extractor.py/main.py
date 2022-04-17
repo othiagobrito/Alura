5bfcfaf3b039f0e7d3d1cefeda06247e4635fe62
Thiago Brito <thiago7brito@gmail.com>
2022-04-17 09:54:06 -0300

    1.5
    Fatiamento de strings

--- Python para Web/String em Python Com OO/url-extractor.py/main.py
+++ Python para Web/String em Python Com OO/url-extractor.py/main.py
@@ -1,2 +1,9 @@
-url = "https://www.google.com/search?q=1+brl+to+usd&rlz=1C1CHWL_enBR897BR897&oq=1+brl+to+usd"
+#url = "https://www.google.com/search?q=1+brl+to+usd&rlz=1C1CHWL_enBR897BR897&oq=1+brl+to+usd"
+url = "google.com/search?q=1+brl+to+usd"
 print(url)
+
+base_url = url[0:17]
+print(base_url)
+
+url_parameters = url[18::]
+print(url_parameters)
