eefeb473a7a5702943ce5a207b367c07369cf7e9
Thiago Brito <thiago7brito@gmail.com>
2022-04-17 20:45:41 -0300

    3,4
    Método abstrato

--- Python para Web/Introdução a Collections/heranca-e-polimorfismo.py
+++ Python para Web/Introdução a Collections/heranca-e-polimorfismo.py
@@ -1,10 +1,17 @@
-class Conta:
+from abc import ABCMeta, abstractmethod
+
+
+class Conta(metaclass=ABCMeta):
     def __init__(self, codigo):
         self._codigo = codigo
         self._saldo = 0
 
     def deposita(self, valor):
         self._saldo += valor
+    
+    @abstractmethod
+    def passa_mes(self):
+        pass
     
     def __str__(self):
         return f"Código: {self._codigo} - Saldo: {self._saldo}"
@@ -19,6 +26,9 @@ class ContaPoupanca(Conta):
     def passa_mes(self):
         self._saldo *= 1.01
         self._saldo -= 3
+
+class ContaInvestimento(Conta):
+    pass
 
 conta16 = ContaCorrente(16)
 conta16.deposita(1000)
