d264b3d0a58d62901954b8de7848c4e03f3d8193
Thiago Brito <thiago7brito@gmail.com>
2022-04-17 20:56:31 -0300

    4.2
    Igualdade e o __eq__

---
+++ Python para Web/Introdução a Collections/igualdade.py
@@ -0,0 +1,26 @@
+class ContaSalario:
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+    
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __eq__(self, outro):
+        if type(outro) != ContaSalario:
+            return False
+
+        return self._codigo == outro._codigo and self._saldo == outro._saldo
+    
+    def __str__(self):
+        return f"Código: {self._codigo} - Saldo: {self._saldo}"
+
+class ContaMultiploSalario(ContaSalario):
+    pass
+    
+conta1 = ContaSalario(37)
+print(conta1)
+conta2 = ContaSalario(37)
+print(conta2)
+
+print(conta1 == conta2)
