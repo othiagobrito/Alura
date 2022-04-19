bbd4623926fb7bee65c6559baff250de7182ef80
Thiago Brito <thiago7brito@gmail.com>
2022-04-19 11:52:48 -0300

    3.3
    Mais operações de dicionários

--- Python para Web/Introdução a Collections P2/mapa.py
+++ Python para Web/Introdução a Collections P2/mapa.py
@@ -14,3 +14,35 @@ print(aparicoes["cachorro"])
 print(aparicoes.get("xpto", 0))
 print(aparicoes.get("cachorro", 0))
 
+aparicoes["Carlos"] = 1
+print(aparicoes)
+
+aparicoes["Carlos"] = 2
+print(aparicoes)
+
+del aparicoes["Carlos"]
+print(aparicoes)
+
+print("cachorro" in aparicoes)
+print("Carlos" in aparicoes)
+
+for elemento in aparicoes:
+    print(elemento)
+
+for elemento in aparicoes.keys():
+    print(elemento)
+
+for elemento in aparicoes.values():
+    print(elemento)
+
+for elemento in aparicoes:
+    valor = aparicoes[elemento]
+    print(elemento, valor)
+
+for elemento in aparicoes.items():
+    print(elemento)
+
+for chave, valor in aparicoes.items():
+    print(f"{chave} = {valor}")
+
+print([f"palavra {chave}" for chave in aparicoes.keys()])
