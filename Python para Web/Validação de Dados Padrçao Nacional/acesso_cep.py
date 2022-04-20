5ec22221bd43a17846666738da09103f82cd12b9
Thiago Brito <thiago7brito@gmail.com>
2022-04-20 20:38:57 -0300

    5.1
    Introducao APIs e validação de CEP

---
+++ Python para Web/Validação de Dados Padrçao Nacional/acesso_cep.py
@@ -0,0 +1,19 @@
+class BuscaEndereco:
+    def __init__(self, cep):
+        cep = str(cep)
+        if self.cep_valido(cep):
+            self.cep = cep
+        else:
+            raise ValueError("CEP inválido!")
+    
+    def __str__(self):
+        return self.format()
+
+    def cep_valido(self, cep):
+        if len(cep) == 8:
+            return True
+        else:
+            return False
+    
+    def format(self):
+        return f"{self.cep[:5]}-{self.cep[5:]}"
---
+++ Python para Web/Validação de Dados Padrçao Nacional/main_cep.py
@@ -0,0 +1,5 @@
+from acesso_cep import BuscaEndereco
+
+cep = 12345012
+obj_cep = BuscaEndereco(cep)
+print(obj_cep)
