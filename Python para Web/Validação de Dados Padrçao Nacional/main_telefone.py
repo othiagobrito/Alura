13a4287919e9c97e00e44bde535d4a092375b820
Thiago Brito <thiago7brito@gmail.com>
2022-04-20 18:00:22 -0300

    3.3
    Criando Máscara para o numero de telefone

--- Python para Web/Validação de Dados Padrçao Nacional/main_telefone.py
+++ Python para Web/Validação de Dados Padrçao Nacional/main_telefone.py
@@ -1,6 +1,8 @@
 from telefones_br import TelefonesBr
 import re
 
-numero = "1126481234"
+numero = "551120305520"
 
 numero_obj = TelefonesBr(numero)
+
+print(numero_obj)
--- Python para Web/Validação de Dados Padrçao Nacional/telefones_br.py
+++ Python para Web/Validação de Dados Padrçao Nacional/telefones_br.py
@@ -7,11 +7,21 @@ class TelefonesBr:
         else:
             raise ValueError("Número incorreto!")
 
+    def __str__(self):
+        return self.format()
+
     def valida(self, numero):
-        padrao = "([0-9]{2,3})([0-9]{2})([0-9]{4,5})(-)?([0-9]{4})"
+        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
         resposta = re.findall(padrao, numero)
 
         if resposta:
             return True
         else:
             return False
+        
+    def format(self):
+        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        resposta = re.search(padrao, self.numero)
+        numero_formatado = f"+{resposta.group(1)} ({resposta.group(2)}) {resposta.group(3)}-{resposta.group(4)}"
+
+        return numero_formatado
