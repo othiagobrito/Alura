0bd0769a4222ac38f1cabd634b4a5544b2d5ae34
Thiago Brito <thiago7brito@gmail.com>
2022-04-28 20:17:12 -0300

    5.3
    Parâmetro na url

--- Django/Modelo,Rotas,Views/aplicacao/receitas/views.py
+++ Django/Modelo,Rotas,Views/aplicacao/receitas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Receita
 
 # Create your views here.
@@ -12,5 +12,11 @@ def index(request):
 
     return render(request, "index.html", dados)
 
-def receita(request):
-    return render(request, "receita.html")
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita': receita,
+    }
+
+    return render(request, "receita.html", receita_a_exibir)
