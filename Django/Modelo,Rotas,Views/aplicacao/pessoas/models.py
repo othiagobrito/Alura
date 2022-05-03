9d41bbd94739eb122577c75c3056a5c6c6c0d25d
Thiago Brito <thiago7brito@gmail.com>
2022-05-03 11:36:26 -0300

    2.3
    Exibindo nome das pessoas

--- Django/Modelo,Rotas,Views/aplicacao/pessoas/models.py
+++ Django/Modelo,Rotas,Views/aplicacao/pessoas/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Pessoa(models.Model):
     nome = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
