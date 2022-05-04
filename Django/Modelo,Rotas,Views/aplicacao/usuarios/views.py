f9db34b50248c94c422ddf1b307ed99ac75f82cf
Thiago Brito <thiago7brito@gmail.com>
2022-05-04 11:41:08 -0300

    1.3
    Criando o app de usuários

---
+++ Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def cadastro(request):
+    pass
+
+def login(request):
+    pass
+
+def dashboard(request):
+    pass
+
+def logout(request):
+    pass
+
