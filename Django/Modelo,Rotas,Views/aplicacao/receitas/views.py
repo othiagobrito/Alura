434f7cc36eac9d5922f44b8ba8b57f74227ce7aa
Thiago Brito <thiago7brito@gmail.com>
2022-05-05 11:39:21 -0300

    3.1
    Refatorando a pasta template

--- Django/Modelo,Rotas,Views/aplicacao/receitas/views.py
+++ Django/Modelo,Rotas,Views/aplicacao/receitas/views.py
@@ -10,7 +10,7 @@ def index(request):
         "receitas": receitas
     }
 
-    return render(request, "index.html", dados)
+    return render(request, "receitas/index.html", dados)
 
 def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
@@ -19,7 +19,7 @@ def receita(request, receita_id):
         'receita': receita,
     }
 
-    return render(request, "receita.html", receita_a_exibir)
+    return render(request, "receitas/receita.html", receita_a_exibir)
 
 def buscar(request):
     lista_receitas = Receita.objects.order_by("-date_receita").filter(publicada=True)
@@ -33,4 +33,4 @@ def buscar(request):
         "receitas": lista_receitas
     }
 
-    return render(request, "buscar.html", dados)
+    return render(request, "receitas/buscar.html", dados)
--- Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
+++ Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
@@ -99,7 +99,7 @@ def cria_receita(request):
 
         return redirect("dashboard")
     else:    
-        return render(request, "usuarios/cria_receita.html")
+        return render(request, "receitas/cria_receita.html")
 
 def deleta_receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
@@ -111,7 +111,7 @@ def edita_receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
     receita_a_editar = {"receita": receita}
     
-    return render(request, "usuarios/edita_receita.html", receita_a_editar)
+    return render(request, "receitas/edita_receita.html", receita_a_editar)
 
 def atualiza_receita(request):
     if request.method == "POST":
