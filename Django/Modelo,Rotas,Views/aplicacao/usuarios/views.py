5b889d07452ad172859fa75b6ec45030abc32408
Thiago Brito <thiago7brito@gmail.com>
2022-05-04 22:52:45 -0300

    4.4
    Receita de cada usuário

--- Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
+++ Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
+from receitas.models import Receita
 
 # Create your views here.
 
@@ -51,7 +52,15 @@ def login(request):
 
 def dashboard(request):
     if request.user.is_authenticated:
-        return render(request, "usuarios/dashboard.html")
+        id = request.user.id
+
+        receitas = Receita.objects.order_by("-date_receita").filter(pessoa=id)
+
+        dados = {
+            "receitas": receitas
+        }
+
+        return render(request, "usuarios/dashboard.html", dados)
     else:
         return redirect("index")
 
@@ -68,6 +77,11 @@ def cria_receita(request):
         rendimento = request.POST["rendimento"]
         categoria = request.POST["categoria"]
         foto_receita = request.FILES["foto_receita"]
+
+        user = get_object_or_404(User, pk=request.user.id)
+
+        receita = Receita.objects.create(pessoa=user, nome_receita=nome_receita, ingredientes=ingredientes, modo_preparo=modo_preparo, tempo_preparo=tempo_preparo, rendimento=rendimento, categoria=categoria, foto_receita=foto_receita)
+        receita.save()
 
         return redirect("dashboard")
     else:    
