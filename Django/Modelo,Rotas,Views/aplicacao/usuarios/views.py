2a82cc166dd4b735b44918bc39e0a48d48600627
Thiago Brito <thiago7brito@gmail.com>
2022-05-04 22:32:03 -0300

    4.3
    Dados da requisição

--- Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
+++ Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
@@ -60,4 +60,15 @@ def logout(request):
     return redirect("index")
 
 def cria_receita(request):
-    return render(request, "usuarios/cria_receita.html")
+    if request.method == "POST":
+        nome_receita = request.POST["nome_receita"]
+        ingredientes = request.POST["ingredientes"]
+        modo_preparo = request.POST["modo_preparo"]
+        tempo_preparo = request.POST["tempo_preparo"]
+        rendimento = request.POST["rendimento"]
+        categoria = request.POST["categoria"]
+        foto_receita = request.FILES["foto_receita"]
+
+        return redirect("dashboard")
+    else:    
+        return render(request, "usuarios/cria_receita.html")
