09390a6e9e72582e209e7da68dc241964b16391c
Thiago Brito <thiago7brito@gmail.com>
2022-05-04 21:17:16 -0300

    3.2
    Realizando o Login

--- Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
+++ Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
@@ -36,8 +37,15 @@ def login(request):
 
         if email == "" or senha == "":
             return redirect("login")
-            
-        return redirect("dashboard")
+        
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list("username", flat=True).get()
+
+            user = auth.authenticate(request, username=nome, password=senha)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect("dashboard")
 
     return render(request, "usuarios/login.html")
 
