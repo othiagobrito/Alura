3487dbe62bfbf12b96f4bfe635755f6fd2f097fd
Thiago Brito <thiago7brito@gmail.com>
2022-05-05 10:25:05 -0300

    1.3
    Deletando receitas

--- Django/Modelo,Rotas,Views/aplicacao/usuarios/urls.py
+++ Django/Modelo,Rotas,Views/aplicacao/usuarios/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("dashboard", views.dashboard, name="dashboard"),
     path("logout", views.logout, name="logout"),
     path("cria/receita", views.cria_receita, name="cria_receita"),
+    path("deleta/<int:receita_id>", views.deleta_receita, name="deleta_receita"),
     
 ]
--- Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
+++ Django/Modelo,Rotas,Views/aplicacao/usuarios/views.py
@@ -101,6 +101,12 @@ def cria_receita(request):
     else:    
         return render(request, "usuarios/cria_receita.html")
 
+def deleta_receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    receita.delete()
+    
+    return redirect("dashboard")
+
 def campo_vazio(campo):
     return not campo.strip()
 
