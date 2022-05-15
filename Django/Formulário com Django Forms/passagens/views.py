637b15c4f4bcf26fbe0b94ed98a7377d461bda7c
Thiago Brito <thiago7brito@gmail.com>
2022-05-15 11:38:20 -0300

    1.4
    Django Form

--- Django/Formulário com Django Forms/passagens/views.py
+++ Django/Formulário com Django Forms/passagens/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from passagens.forms import PassagemForms
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    form = PassagemForms()
+
+    contexto = {
+        "form": form
+    }
+
+    return render(request, "index.html", contexto)
