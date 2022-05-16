a74f95bc9d020fa2c3258d65a428562f4d569886
Thiago Brito <thiago7brito@gmail.com>
2022-05-16 11:01:14 -0300

    3.4
    Criando o app

--- Django/Fundamentos-do-TDD/tdd_busca_animal/setup/tests.py
+++ Django/Fundamentos-do-TDD/tdd_busca_animal/setup/tests.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class AnimaisTestCase(LiveServerTestCase):
     def setUp(self):
@@ -8,8 +9,10 @@ class AnimaisTestCase(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_abre_janela_chrome(self):
-        self.browser.get(self.live_server_url)
-
     def test_buscando_animal(self):
         ''' Teste que simula busca de animal na pesquisa por um usuário '''
+
+        home_page = self.browser.get(self.live_server_url + "/")
+
+        brand_element = self.browser.find_element(By.CSS_SELECTOR, ".navbar")
+        self.assertEqual("Busca Animal", brand_element.text)
