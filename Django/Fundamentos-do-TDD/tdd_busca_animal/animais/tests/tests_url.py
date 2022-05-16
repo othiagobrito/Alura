50e2282e5ed5c75732454bbe23b85a9390706d66
Thiago Brito <thiago7brito@gmail.com>
2022-05-16 11:24:10 -0300

    3.6
    RequestFactory

--- Django/Fundamentos-do-TDD/tdd_busca_animal/animais/tests/tests_url.py
+++ Django/Fundamentos-do-TDD/tdd_busca_animal/animais/tests/tests_url.py
@@ -1,11 +1,16 @@
-import imp
-from django.test import TestCase
+from urllib import request
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
 from animais.views import index
 
 class AnimaisURLSTestCase(TestCase):
-    ''' Teste para verificar se home da aplicação utilização função index da view '''
+
+    def setUp(self):
+        self.factory = RequestFactory()
 
     def test_rota_url_utiliza_view_index(self):
-        root = reverse("/")
-        self.assertEqual(root.func, index)
+        ''' Teste para verificar se home da aplicação utilização função index da view '''
+        
+        request = self.factory.get("/")
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
