514cf4f84939082224623c6eab9ee7ad00cea81c
Thiago Brito <thiago7brito@gmail.com>
2022-05-30 09:51:01 -0300

    2.6
    Tipos de dados

---
+++ PHP/Primeiros-Passos/tipos.php
@@ -0,0 +1,22 @@
+<?php
+
+$idade = 24;
+echo gettype($idade) . "\n";
+
+$salario = 1000.3;
+echo gettype($salario) . "\n";
+
+$divisao = 10 / 3;
+echo $divisao . "\n";
+echo gettype($divisao) . "\n";
+
+$texto = "Olá, mundo!";
+echo gettype($texto) . "\n";
+
+$verdadeiro = true;
+echo gettype($verdadeiro) . "\n";
+
+$falso = false;
+echo gettype($falso) . "\n";
+
+?>
