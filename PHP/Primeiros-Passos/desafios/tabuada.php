ce41ee932c7ec112f4e455d868d4e1f4d699d83f
Thiago Brito <thiago7brito@gmail.com>
2022-05-30 11:16:53 -0300

    6.3
    Solução e desafio Tabuada

---
+++ PHP/Primeiros-Passos/desafios/tabuada.php
@@ -0,0 +1,10 @@
+<?php
+
+$numero = 2;
+
+for ($multiplicador = 1; $multiplicador <= 10; $multiplicador++) {
+    $resultado = $numero * $multiplicador;
+    echo "$numero x $multiplicador = $resultado\n";
+}
+
+?>
