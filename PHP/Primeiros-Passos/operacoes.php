cbcc3aba2768ec6282b74760837ffebdccbfed44
Thiago Brito <thiago7brito@gmail.com>
2022-05-30 09:41:46 -0300

    2.4
    Operações matemáticas

---
+++ PHP/Primeiros-Passos/operacoes.php
@@ -0,0 +1,18 @@
+<?php
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$doisAoCubo = 2 ** 2;
+$restoDivisao = 10 % 3;
+
+echo "$soma\n";
+echo "$subtracao\n";
+echo "$multiplicacao\n";
+echo "$divisao\n";
+echo "$doisAoCubo\n";
+echo "$restoDivisao\n";
+
+?>
