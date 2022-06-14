85f383174ef5a00e052a12fb0ffe7f19399637ab
thiagobrito <thiago7brito@gmail.com>
2022-06-14 10:28:18 -0300

    2.2 Ordenação simples

---
+++ PHP/PHP-arrays/ordem.php
@@ -0,0 +1,12 @@
+<?php
+
+$notas = [10,8,9,7];
+
+$notasOrdenadas = $notas;
+sort($notasOrdenadas);
+
+echo "Desordenadas:";
+var_dump($notas);
+
+echo "Ordenadas: $notasOrdenadas";
+var_dump($notasOrdenadas);
