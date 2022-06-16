ed18506d1308e3a83dcb93b1cbdb371dce5d945b
thiagobrito <thiago7brito@gmail.com>
2022-06-16 16:25:07 -0300

    4.4 Obtendo o que importa

--- PHP/PHP-arrays/bimestres.php
+++ PHP/PHP-arrays/bimestres.php
@@ -10,9 +10,14 @@ $notasBimestre1 = [
 
 $notasBimestre2 = [
     'João' => 8,
-    'Ana' => 10,
+    'Ana' => 9,
     'Roberto' => 7,
-    'Maria' => 9,
 ];
 
-var_dump(array_diff_key($notasBimestre1, $notasBimestre2));
+$alunos_falta = array_diff_key($notasBimestre1, $notasBimestre2);
+
+$nomeAlunos = array_keys($alunos_falta);
+$notaAlunos = array_values($alunos_falta);
+
+var_dump(array_combine($notaAlunos, $nomeAlunos));
+var_dump(array_flip($alunos_falta));
