b493673dbd57618dde3916d6eb81bd71898aa852
thiagobrito <thiago7brito@gmail.com>
2022-06-19 14:18:27 -0300

    2.3 Criando um autoloader

---
+++ PHP/Strings/autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+spl_autoload_register(function ($classe) {
+    $prefixo = "App\\";
+
+    $diretorio = __DIR__ . '/src/';
+
+    if (strncmp($prefixo, $classe, strlen($prefixo)) !== 0) {
+        return;
+    }
+    
+});
