0c73ac45c8becd0990b5d03b8a229d987a2b9177
thiagobrito <thiago7brito@gmail.com>
2022-06-19 13:11:50 -0300

    1.6 Adicionando checagem no formulário

--- PHP/Strings/usuario.php
+++ PHP/Strings/usuario.php
@@ -12,8 +12,17 @@ class Usuario
     {
         $nomeSobrenome = explode(" ", $nome, 2);
 
-        $this->nome = $nomeSobrenome[0];
-        $this->sobrenome = $nomeSobrenome[1];
+        if ($nomeSobrenome[0] === "") {
+            $this->nome = "Nome inválido";
+        } else {
+            $this->nome = $nomeSobrenome[0];
+        }
+
+        if ($nomeSobrenome[1] === null) {
+            $this->sobrenome = "Sobrenome inválido";
+        } else {
+            $this->sobrenome = $nomeSobrenome[1];
+        }
     }
 
     public function getNome(): string
