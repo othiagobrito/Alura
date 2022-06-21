caea3975ca4b8e84e9154acd9159f96efc8d3d2a
thiagobrito <thiago7brito@gmail.com>
2022-06-21 14:51:07 -0300

    3.4 Revisando o conteúdo

--- PHP/projeto-php-oo/src/conta.php
+++ PHP/projeto-php-oo/src/conta.php
@@ -5,7 +5,7 @@ class Conta
 
     public string $cpfTitular;
     public string $nometitular;
-    public float $saldo;
+    public float $saldo = 0;
 
     public function sacar(float $valor)
     {
@@ -13,6 +13,15 @@ class Conta
             echo "Saldo indisponível!";
         } else {
             $this->saldo -= $valor;
+        }
+    }
+
+    public function depositar(float $valor)
+    {
+        if ($valor <= 0) {
+            echo "Valor de depósito deve ser positivo";
+        } else {
+            $this->saldo += $valor;
         }
     }
 
