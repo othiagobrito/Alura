0155999f92a48f0de695adbdcc628b340b4e7d67
thiagobrito <thiago7brito@gmail.com>
2022-06-21 14:42:05 -0300

    3.2 Criando o nosso primeiro método

--- PHP/projeto-php-oo/src/conta.php
+++ PHP/projeto-php-oo/src/conta.php
@@ -7,4 +7,13 @@ class Conta
     public string $nometitular;
     public float $saldo;
 
+    public function sacar(float $valor)
+    {
+        if ($valor > $this->saldo) {
+            echo "Saldo indisponível!";
+        } else {
+            $this->saldo -= $valor;
+        }
+    }
+
 }
