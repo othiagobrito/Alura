0628100b16106c1e3efffd575c0c5829df4cc1e8
thiagobrito <thiago7brito@gmail.com>
2022-06-22 08:15:54 -0300

    4.4 Métodos de acesso

--- PHP/projeto-php-oo/banco.php
+++ PHP/projeto-php-oo/banco.php
@@ -3,6 +3,10 @@
 require_once 'src/Conta.php';
 
 $conta1 = new Conta;
-$conta1->sacar(1500);
+$conta1->setNomeTitular("Thiago Brito");
+$conta1->setCpf("123.456.789-01");
+$conta1->depositar(1500);
 
-var_dump($conta1);
+echo $conta1->getNome() . PHP_EOL;
+echo $conta1->getCpf() . PHP_EOL;
+echo $conta1->extrato() . PHP_EOL;
--- PHP/projeto-php-oo/src/conta.php
+++ PHP/projeto-php-oo/src/conta.php
@@ -3,17 +3,17 @@
 class Conta
 {
 
-    private $cpfTitular;
-    private $nometitular;
+    private $cpfTitular = "";
+    private $nometitular = "";
     private $saldo = 0;
 
     public function sacar(float $valor): void
     {
         if ($valor > $this->saldo) {
-            echo "Saldo indisponível!";
+            echo "Saldo indisponível!" . PHP_EOL;
             return;
         } elseif ($valor <= 0) {
-            echo "Valor de saque inválido!";
+            echo "Valor de saque inválido!" . PHP_EOL;
             return;
         }
         
@@ -23,7 +23,7 @@ class Conta
     public function depositar(float $valor): void
     {
         if ($valor <= 0) {
-            echo "Valor de depósito deve ser positivo";
+            echo "Valor de depósito deve ser positivo" . PHP_EOL;
             return;
         }
         
@@ -33,12 +33,37 @@ class Conta
     public function transferir(float $valor, Conta $destino): void
     {
         if ($valor > $this->saldo) {
-            echo "Saldo indisponível!";
+            echo "Saldo indisponível!" . PHP_EOL;
             return;
         }
 
         $this->sacar($valor);
         $destino->depositar($valor);
+    }
+
+    public function extrato(): string
+    {
+        return "Saldo: {$this->saldo}";
+    }
+
+    public function setNomeTitular(string $nome): void
+    {
+        $this->nometitular = $nome;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nometitular;
+    }
+
+    public function setCpf(string $cpf): void
+    {
+        $this->cpfTitular = $cpf;
+    }
+    
+    public function getCpf(): string
+    {
+        return $this->cpfTitular;
     }
 
 }
