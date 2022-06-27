502593ae6e372b4c00c1b10b28a292566cff8d59
thiagobrito <thiago7brito@gmail.com>
2022-06-27 09:56:10 -0300

    7.7 Autoload de classes

---
+++ PHP/projeto-php-oo/autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+spl_autoload_register(function (string $nomeCompletoClasse)
+    {
+        $caminhoArquivo = str_replace('Alura\\Banco', 'src', $nomeCompletoClasse);
+        $caminhoArquivo = str_replace('\\', DIRECTORY_SEPARATOR, $caminhoArquivo);
+        $caminhoArquivo = $caminhoArquivo . '.php';
+
+        if (file_exists($caminhoArquivo)) {
+            require_once $caminhoArquivo;
+        }
+    }
+);
