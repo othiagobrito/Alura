944aee4f1e2eb817645abe5a5a2ab0d15dd35fcd
thiagobrito <thiago7brito@gmail.com>
2022-07-05 14:05:29 -0300

    2.5 Facilitando a escrita

--- PHP/PHP-IO-Trabalhando-com-arquivos-e-streams/escritor-cursos.php
+++ PHP/PHP-IO-Trabalhando-com-arquivos-e-streams/escritor-cursos.php
@@ -1,8 +1,5 @@
 <?php
 
-$arquivo = fopen("cursos-php.txt", "a");
-
 $curso = "\nCurso de CSS";
-fwrite($arquivo, $curso);
 
-fclose($arquivo);
+file_put_contents("cursos-php.txt", $curso, FILE_APPEND);
