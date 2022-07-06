5b3d2f568a56987e0c655619653d38fb452643f8
thiagobrito <thiago7brito@gmail.com>
2022-07-05 22:17:16 -0300

    3.7 Criando um filtro

--- PHP/PHP-IO-Trabalhando-com-arquivos-e-streams/filtros.php
+++ PHP/PHP-IO-Trabalhando-com-arquivos-e-streams/filtros.php
@@ -1,8 +1,11 @@
 <?php
 
+require "MeuFiltro.php";
+
 $arquivosCursos = fopen("lista-cursos.txt", "r");
 
-stream_filter_append($arquivosCursos, "string.toupper");
+stream_filter_register("alura.partes", MeuFiltro::class);
+stream_filter_append($arquivosCursos, "alura.partes");
 
 echo fread($arquivosCursos, filesize("lista-cursos.txt"));
 
