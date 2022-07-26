98d87c36d0546ea9d701a59e23fe0227e1abed09
thiagobrito <thiago7brito@gmail.com>
2022-07-25 22:56:26 -0300

    7.4 UTF-8

--- PHP/PHP-IO-Trabalhando-com-arquivos-e-streams/arquivos-para-csv.php
+++ PHP/PHP-IO-Trabalhando-com-arquivos-e-streams/arquivos-para-csv.php
@@ -6,14 +6,14 @@ $outrosCursos = file('cursos-php.txt');
 $arquivoCsv = fopen('cursos.csv', 'w');
 
 foreach ($meusCursos as $curso) {
-    $linha = [trim($curso), 'Sim'];
+    $linha = [trim(utf8_decode($curso)), 'Sim'];
 
-    fputcsv($arquivoCsv, $linha, ',');
+    fputcsv($arquivoCsv, $linha, ';');
 }
 
 foreach ($outrosCursos as $curso) {
-    $linha = [trim($curso), 'Não'];
-    fputcsv($arquivoCsv, $linha, ',');
+    $linha = [trim(utf8_decode($curso)), 'Não'];
+    fputcsv($arquivoCsv, $linha, ';');
 }
 
 fclose($arquivoCsv);
