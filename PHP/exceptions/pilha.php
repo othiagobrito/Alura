6f5f74712f103a4c69bdf5c8958f88dedfb5e404
thiagobrito <thiago7brito@gmail.com>
2022-08-26 21:14:04 -0300

    2.6 Pegando na pilha

--- PHP/exceptions/pilha.php
+++ PHP/exceptions/pilha.php
@@ -5,21 +5,23 @@ function funcao1()
     echo 'Entrei na função 1' . PHP_EOL;
 
     try {
-        $arrayFixo = new SplFixedArray(2);
-        $arrayFixo[3] = 'Valor';
+        funcao2();
     } catch (RuntimeException $erro) {
-        echo 'Aconteceu um erro na função 1' . PHP_EOL;
+        echo 'Aconteceu um erro na função 2' . PHP_EOL;
     }
 
     // $divisao = intdiv(5, 0);
 
-    funcao2();
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
+
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
