e39bfbb9c6b9f0d0f4692faaaaec7fdc8d362464
thiagobrito <thiago7brito@gmail.com>
2022-08-26 23:49:54 -0300

    3.3 Lançando uma exceção

--- PHP/exceptions/pilha.php
+++ PHP/exceptions/pilha.php
@@ -10,6 +10,9 @@ function funcao1()
         echo $error->getMessage() . PHP_EOL;
         echo $error->getLine() . PHP_EOL;
         echo $error->getTraceAsString() . PHP_EOL;
+
+        throw new RuntimeException("Exceção tratada, segue segunda", 1, $error);
+        
     }
 
     echo 'Saindo da função 1' . PHP_EOL;
@@ -19,14 +22,12 @@ function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
 
-    $arrayFixo = new SplFixedArray(2);
-    $arrayFixo[3] = 'Valor';
-
-    $divisao = intdiv(5, 0);
-
+    throw new RuntimeException();
+    
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
+
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
