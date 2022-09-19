07f5bc02e0aa26b0dac2653d993d06f45f6900c4
thiagobrito <thiago7brito@gmail.com>
2022-09-19 15:19:24 -0300

    6.8 Tratamento de erros

--- PHP/exceptions/erros.php
+++ PHP/exceptions/erros.php
@@ -2,8 +2,17 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-ini_set('log_errors', 1);
-ini_set('error_log', '/var/log/minha-aplicacao');
+
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    switch ($errno) {
+        case E_WARNING:
+            echo 'Aviso: Isso é perigoso!' . PHP_EOL;
+            break;
+        case E_NOTICE:
+            echo 'Melhor não fazer isso.' . PHP_EOL;
+            break;
+    }
+});
 
 echo $var;
 echo $array[12];
