4aec3f4e8920cbf6314e205ca8a1adc41257a708
thiagobrito <thiago7brito@gmail.com>
2022-09-19 15:10:48 -0300

    6.6 Configurações de erros

--- PHP/exceptions/erros.php
+++ PHP/exceptions/erros.php
@@ -1,5 +1,10 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', '/var/log/minha-aplicacao');
+
 echo $var;
 echo $array[12];
 // echo CONSTANT;
