a4ee45134ed9c9d9bd2c7fcd8f87394cba811e86
thiagobrito <thiago7brito@gmail.com>
2022-10-03 08:29:06 -0300

    4.2 Instalando PHPUnit

--- PHP/PHP Composer/buscador-cursos-alura/vendor/composer/autoload_psr4.php
+++ PHP/PHP Composer/buscador-cursos-alura/vendor/composer/autoload_psr4.php
@@ -12,9 +12,12 @@ return array(
     'Symfony\\Component\\CssSelector\\' => array($vendorDir . '/symfony/css-selector'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-factory/src', $vendorDir . '/psr/http-message/src'),
     'Psr\\Http\\Client\\' => array($vendorDir . '/psr/http-client/src'),
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
     'Masterminds\\' => array($vendorDir . '/masterminds/html5/src'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
+    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'Alura\\BuscadorDeCursos\\' => array($baseDir . '/src'),
 );
