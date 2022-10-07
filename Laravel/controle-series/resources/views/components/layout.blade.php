014da8554760b40a2c3bfef94587b50df7ac8dc6
thiagobrito <thiago7brito@gmail.com>
2022-10-07 10:28:48 -0300

    4.6 Instalando o Bootstrap

--- Laravel/controle-series/resources/views/components/layout.blade.php
+++ Laravel/controle-series/resources/views/components/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} - Controle de Séries</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <h1>{{ $title }}:</h1>
