1bc1143a24207a15fde3422334e423a1b943a3a7
thiagobrito <thiago7brito@gmail.com>
2022-10-12 14:39:38 -0300

    2.4 Extraindo uma interface

--- Laravel/controle-series/app/Http/Controllers/SeriesController.php
+++ Laravel/controle-series/app/Http/Controllers/SeriesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\{Series, Season, Episode};
+use App\Models\{Series};
 use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
--- Laravel/controle-series/app/Repositories/SeriesRepository.php
+++ Laravel/controle-series/app/Repositories/SeriesRepository.php
@@ -3,44 +3,9 @@
 namespace App\Repositories;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\{Episode, Season, Series};
-use Illuminate\Support\Facades\DB;
+use App\Models\Series;
 
-class SeriesRepository
+interface SeriesRepository
 {
-    public function add(SeriesFormRequest $request): Series
-    {
-        return DB::transaction(function () use ($request, &$series) {
-            $series = Series::create($request->all());
-
-            $seasons = [];
-
-            for ($i = 1; $i <= $request->seasons; $i++) { 
-                $seasons[] = [
-                    'series_id' => $series->id,
-                    'number' => $i,
-                ];
-            }
-
-            Season::insert($seasons);
-
-            $episodes = [];
-
-            foreach ($series->seasons as $season) {
-                for ($j = 1; $j <= $request->episodes; $j++) { 
-                    $episodes[] = [
-                        'season_id' => $season->id,
-                        'number' => $j,
-                    ];
-                }
-            }
-
-            Episode::insert($episodes);
-
-            return $series;
-        }, 5);
-
-        // Outra forma:
-        // DB::beginTransaction(); , DB::commit(); , DB::rollBack();
-    }
+    public function add(SeriesFormRequest $request): Series;
 }
