2d4b0c10dd62a3ce7062606ad38f74b4d5e2df86
thiagobrito <thiago7brito@gmail.com>
2022-10-13 22:57:26 -0300

    5.5 Breeze

--- Laravel/controle-series/routes/web.php
+++ Laravel/controle-series/routes/web.php
@@ -16,23 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('/series', SeriesController::class)->except(['show']);
 
-Route::middleware('authenticator')->group(function () {
-    Route::get('/', function () {
-        return to_route('series.index');
-    })->middleware('authenticator');
-    
-    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index')->middleware('authenticator');
-    
-    Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-    Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
-});
+// Route::get('/login', [LoginController::class, 'index'])->name('login');
+// Route::post('/login', [LoginController::class, 'store'])->name('sign');
+// Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store'])->name('sign');
-Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
-
-Route::get('/register', [UsersController::class, 'index'])->name('users.index');
-Route::post('/register', [UsersController::class, 'store'])->name('users.store');
+// Route::get('/register', [UsersController::class, 'index'])->name('users.index');
+// Route::post('/register', [UsersController::class, 'store'])->name('users.store');
 
 // Route::controller(SeriesController::class)->group(function () {
 //     Route::get('/series', 'index')->name('series.index');
@@ -40,3 +29,23 @@ Route::post('/register', [UsersController::class, 'store'])->name('users.store')
 //     Route::post('/series/store', 'store')->name('series.store');
 // });
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
