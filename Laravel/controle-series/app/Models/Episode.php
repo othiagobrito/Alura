5ae7bb0e70a86644ea0e24cb4a60ec91e2e6d0bd
thiagobrito <thiago7brito@gmail.com>
2023-02-20 20:13:53 -0300

    3.8 Acessors e Mutators

--- Laravel/controle-series/app/Models/Episode.php
+++ Laravel/controle-series/app/Models/Episode.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,10 +16,22 @@ class Episode extends Model
     
     public $timestamps = false;
 
+    protected $casts = [
+        'watched' => 'bool',
+    ];
+
     public function season()
     {
         return $this->belongsTo(Season::class);
     }
+
+    // protected function watched(): Attribute
+    // {
+    //     return new Attribute(
+    //         get: fn ($watched) => (bool) $watched,
+    //         set: fn ($watched) => (bool) $watched,
+    //     );
+    // }
     
     public function scopeWatched($query)
     {
