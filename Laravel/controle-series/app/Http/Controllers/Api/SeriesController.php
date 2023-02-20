9e278d27a8e5997730fe787513ae17b7749ae196
thiagobrito <thiago7brito@gmail.com>
2023-02-20 17:58:23 -0300

    2.8 GET de série

--- Laravel/controle-series/app/Http/Controllers/Api/SeriesController.php
+++ Laravel/controle-series/app/Http/Controllers/Api/SeriesController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+        # code...
+    }
+
     public function index()
     {
         return response()->json(Series::all(), 200);
@@ -16,6 +22,11 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        return response(Series::create($request->all()), 201);
+        return response($this->seriesRepository->add($request), 201);
+    }
+
+    public function show(int $id)
+    {
+        return response()->json(Series::whereId($id)->with('seasons.episodes')->first(), 200);
     }
 }
