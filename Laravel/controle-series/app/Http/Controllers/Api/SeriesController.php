91f2997046dfaf67f9e2d52e403fe798f1dee1ec
thiagobrito <thiago7brito@gmail.com>
2023-02-20 17:00:27 -0300

    1.4 Nosso primeiro Endpoint

---
+++ Laravel/controle-series/app/Http/Controllers/Api/SeriesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Series;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Series::all(), 200);
+    }
+}
